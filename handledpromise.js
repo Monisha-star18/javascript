8815a9a3fbc30cb176a1ff86a47a9824b27b1db4
Monisha-star18 <monisha.r180505@gmail.com>
2026-08-21 12:10:59 +0530

    promise handling with catch then finally

--- handledpromise.js
+++ handledpromise.js
@@ -1,4 +1,4 @@
-isHalwaThere = true
+isHalwaThere = false
 
 function waitInLine(){
     return new Promise ((resolve,reject)=>{
@@ -18,5 +18,51 @@ function waitInLine(){
 function buyHalwa(){
     const wait = waitInLine()
 
-    // wait.then((message)=>{console.})
+    wait
+        .then((message)=>{console.log(message)})
+        .catch( (error)=>{console.log(error)})
+        .finally(()=>{console.log('go home')} )
 }
+
+buyHalwa()
+
+/*
+Time 0ms:  buyHalwa() called
+           │
+           ▼
+Time 0ms:  waitInLine() called
+           │
+           ▼
+Time 0ms:  Promise created (PENDING)
+           │   isHalwaThere = false
+           │   setTimeout(1000ms) starts
+           │
+           ▼
+Time 0ms:  Promise returned to wait variable
+           │   const wait = Promise { <pending> }
+           │
+           ▼
+Time 0ms:  .then() attached (waits for resolve)
+           │   .catch() attached (waits for reject)
+           │   .finally() attached (waits for either)
+           │
+           ▼
+Time 0ms:  buyHalwa() returns (no output yet)
+
+           ⏰ Waiting...
+
+Time 1000ms: ⏰ setTimeout fires!
+           │   isHalwaThere = false
+           │   └── reject('no halwa left') ❌
+           │
+           ▼
+Time 1000ms: .catch() executes
+           │   console.log('no halwa left')
+           │
+           ▼
+Time 1000ms: .finally() executes
+           │   console.log('go home')
+           │
+           ▼
+         COMPLETE
+*/
