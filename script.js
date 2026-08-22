282802d36a21c757598b5c88e4681683d279e79f
Monisha-star18 <monisha.r180505@gmail.com>
2026-08-22 17:03:34 +0530

    crud

--- script.js
+++ script.js
@@ -1,25 +1,58 @@
+
 import {baseUrl} from './shared.js' 
 
-
 $("#addTask-btn").on('click',function(){
-    let inputValue = $("#todo-input").val()
-    console.log(inputValue)
-
-    
-    $(".display-container").prepend
-    (
-                `<div class="card m-4 p-3 bg-primary text-light">
-                    <div class="card-body">
-                        <h1 class="card-title">${inputValue}</h1>
-                        <div class="btnGroup">
-                            <button class="btn btn-warning">Edit the task</button>
-                            <button class="btn btn-danger">Delete</button>
-                        </div>
-                    </div>
-                </div>`
-    )
+    addTask()
 })  
 
 $("#clear-btn").on('click',function(){
     $("#todo-input").val('')
 })
+
+async function addTask()
+    {
+        let inputValue = $("#todo-input").val()
+
+        let taskDetails = {
+        taskName : inputValue ,
+        dateOfCreation : new Date()
+        }
+
+        try
+        {
+            const postResult = await fetch(`${baseUrl}/task`,
+                {
+                method : 'POST',
+                headers:{'Content-Type':'application/json',},
+                body : JSON.stringify(taskDetails)
+                })
+
+            if(postResult.status != 201)
+            {
+                 throw new Error ('cannot post')
+            }
+            else
+            {
+                console.log(postResult)
+                alert('posted successfully')
+                $(".display-container").prepend
+                (
+                            `<div class="card m-4 p-3 bg-primary text-light">
+                                <div class="card-body">
+                                    <h1 class="card-title">${inputValue}</h1>
+                                    <div class="btnGroup">
+                                        <button class="btn btn-warning">Edit the task</button>
+                                        <button class="btn btn-danger">Delete</button>
+                                    </div>
+                                </div>
+                            </div>`
+                )
+            }
+        }
+        catch(err)
+        {
+            alert('Cannot post')
+        }
+        
+
+    }
